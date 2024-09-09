adfeaa6a35ae7336d72f7d508342060f0ae336ff
CarpinchoRecu <franciscocostanzo663@gmail.com>
2024-09-08 23:19:27 -0300

    nueva implementacion en pedidos
    Quiero que se pueda volver a imprimir un pedido ya que puede surgir un fallo en la impresion y quiero que se pueda volver a hacer

--- stock/src/renderer/src/modules/Components/Table/TablesProductos.jsx
+++ stock/src/renderer/src/modules/Components/Table/TablesProductos.jsx
@@ -1,10 +1,12 @@
+import BtnGeneral from '../Btns/BtnGeneral'
+
 const matchesFilter = (value, filter) => {
   if (!filter) return true // Si no hay filtro, siempre devolver verdadero
   // Verificar si el valor comienza con el filtro, considerando minúsculas para la comparación
   return value?.toString().toLowerCase().startsWith(filter.toLowerCase())
 }
 
-const TablesProductos = ({ data = [], filters = {}, ventas }) => {
+const TablesProductos = ({ data = [], filters = {}, ventas, pedidos, handleVolverAImprimir }) => {
   if (!data || data.length === 0)
     return <>{ventas ? <div>No hay ventas cargadas</div> : <div>No hay datos para mostrar</div>}</>
 
@@ -29,11 +31,25 @@ const TablesProductos = ({ data = [], filters = {}, ventas }) => {
       </thead>
       <tbody>
         {filteredData.map((row, index) => (
-          <tr key={index}>
-            {columns.map((column) => (
-              <td key={column}>{row[column] !== undefined ? row[column] : 'N/A'}</td> // Manejar valores undefined
-            ))}
-          </tr>
+          <>
+            {pedidos && (
+
+              <BtnGeneral tocar={handleVolverAImprimir} claseBtn="btn__volver__imprimir">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M17 17h2a2 2 0 0 0 2 -2v-4a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v4a2 2 0 0 0 2 2h2" />
+                  <path d="M17 9v-4a2 2 0 0 0 -2 -2h-6a2 2 0 0 0 -2 2v4" />
+                  <path d="M7 13m0 2a2 2 0 0 1 2 -2h6a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-6a2 2 0 0 1 -2 -2z" />
+                </svg>
+                Volver a Imprimir
+              </BtnGeneral>
+            )}
+            <tr key={index}>
+              {columns.map((column) => (
+                <td key={column}>{row[column] !== undefined ? row[column] : 'N/A'}</td> // Manejar valores undefined
+              ))}
+            </tr>
+          </>
         ))}
       </tbody>
     </table>
--- stock/src/renderer/src/modules/Pedidos/VerPedidos.jsx
+++ stock/src/renderer/src/modules/Pedidos/VerPedidos.jsx
@@ -54,44 +54,9 @@ const VerPedidos = () => {
               onFilterChange={handleFilterChange}
             />
             <div className="table-wrapper">
-              <TablesProductos data={pedidos} filters={filters} />
+              <TablesProductos pedidos={true} data={pedidos} filters={filters} />
             </div>
           </article>
-          {/* {user.rol === 'admin' && (
-            <>
-              <ToolsMercaderia />
-              <article className="contenedor__btns__sigpestanas">
-                <Link className="btn__pestanas__siguiente" to="/fallas">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M11 14l4 -4l-4 -4" />
-                    <path d="M16 14l4 -4l-4 -4" />
-                    <path d="M15 10h-7a4 4 0 1 0 0 8h1" />
-                  </svg>
-                  Fallas
-                </Link>
-
-                <Link className="btn__pestanas__siguiente" to="/stock">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M11 14l4 -4l-4 -4" />
-                    <path d="M16 14l4 -4l-4 -4" />
-                    <path d="M15 10h-7a4 4 0 1 0 0 8h1" />
-                  </svg>
-                  Stock
-                </Link>
-                <Link className="btn__pestanas__siguiente" to="/inversion">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M11 14l4 -4l-4 -4" />
-                    <path d="M16 14l4 -4l-4 -4" />
-                    <path d="M15 10h-7a4 4 0 1 0 0 8h1" />
-                  </svg>
-                  Inversion
-                </Link>
-              </article>
-            </>
-          )} */}
         </>
       )}
     </section>
